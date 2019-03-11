ffe6aae22ed4eb3e91a4b4502a03aabb6d969f22
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-03-11 12:38:51 +0100

    Cleaner functions; Added report sample-data

--- index.js
+++ index.js
@@ -1,92 +1,100 @@
 // Import env from .env
 require('dotenv').config()
-const axios = require("axios");
-const fs = require('fs')
+const axios = require('axios')
+const errorHandler = require('./lib/errorHandler')
 
+let basicAuth = Buffer.from(process.env.USERNAME + ':' + process.env.PASSWORD, 'utf-8').toString('base64')
 
-
-let basicAuth = Buffer.from(process.env.USERNAME + ":" + process.env.PASSWORD, "utf-8").toString("base64");
-
-let apiUrl = "https://***REMOVED***/webacs/api/v3/data/";
+let apiUrl = 'https://***REMOVED***/webacs/api/v3/data/';
 
 let headers = {
   headers: {
-    Authorization: "Basic " + basicAuth
+    Authorization: 'Basic ' + basicAuth
   }
-};
+}
 
-function getClientIds(apiUrl, headers) {
-  return new Promise(async function(fulfill, reject) {
-    let resource = "Clients.json?"
+/**
+ * Returns a promise with IDs of clients
+ * @param  {String} apiUrl URL to the API including path.
+ * @param  {Object} headers Headers to include in the request.
+ * @param  {String} headers.Authorization A basic auth string.
+ * @return {Promise<array>} Client IDs.
+ */
+function getClientIds (apiUrl, headers) {
+  return new Promise(async function (resolve, reject) {
+    let resource = 'Clients.json?'
     let query = [
-      ".firstResult=0",
-      ".maxResults=20",
-      "vlanId=in(208,%20104)",
-      "securityPolicyStatus=%22PASSED%22"
-    ].join("&");
+      '.firstResult=0',
+      '.maxResults=20',
+      'vlanId=in(208,%20104)',
+      'securityPolicyStatus=%22PASSED%22'
+    ].join('&')
+
+    // ### SAMPLE ###
+    /*
+
+    let userList = require('./sample-data/user-list.json')
+    userList = userList.data.queryResponse.entityId
+    resolve(userList.map(user => user.$))
+
+    // */
+    // ### SAMPLE END ###
 
     axios.get(apiUrl + resource + query, headers)
-    .then(response => {
-      let userList = response.data.queryResponse.entityId
-      fulfill(userList.map(user => user.$))
-    })
-    .catch(err => {
-      reject(err)
-    })
+      .then(response => {
+        let userList = response.data.queryResponse.entityId
+        resolve(userList.map(user => user.$))
+      })
+      .catch(err => {
+        reject(err)
+      })
   })
 }
 
-function getClientById(apiUrl, clientId) {
-  return new Promise(async function(fulfill, reject) {
-    let resource = "Clients/" + clientId + ".json?"
+/**
+ * Returns a promise with IDs of clients
+ * @param  {String} apiUrl URL to the API including path.
+ * @param  {Object} headers Headers to include in the request.
+ * @param  {String} headers.Authorization A basic auth string.
+ * @param  {String} clientId
+ * @return {Promise<array>} Client IDs.
+ */
+function getClientById (apiUrl, headers, clientId) {
+  return new Promise(async function (resolve, reject) {
+    let resource = 'Clients/' + clientId + '.json?'
 
-    axios.get(apiUrl + resource, headers)
-    .then(response => {
-      let userList = response.data.queryResponse.entityId
-      fulfill(userList.map(user => user.$))
-    })
-    .catch(err => {
-      reject(err)
-    })
+    let userList = {}
 
-  })
-}
-
-
-
-
-
-
-async function getUsers(fullQuery, headers) {
-  return new Promise(async function(fulfill, reject) {
-    let userList = await Promise.resolve(
-      axios.get(fullQuery, headers)
-    ).catch(err => {
-      reject(err)
-    });
-
-
-    fs.writeFile("./sample-data/user-list.json", JSON.stringify(userList.data.queryResponse), (err => {reject(err)}))
-    let requests = []
+    // ### SAMPLE ###
+    /*
+    try {
+      userList = require('./sample-data/clients/' + clientId + '.json')
+    } catch (error) {
+      errorHandler(error)
+      return 1
+    }
 
     userList = userList.data.queryResponse.entityId
+    resolve(userList.map(user => user.$))
 
+    // */
+    // ### SAMPLE END ###
 
-    userList.forEach(await async function (user) {
-      let userUrl = user['@url']
-      await axios.get(userUrl, headers)
-      .then(userInfo => {
-        userInfo = userInfo.data.queryResponse
-        fs.writeFile("./sample-data/clients/" + user.$ + ".json", JSON.stringify(userInfo), (err => {reject(err)}))
-        console.log(user.$)
+    axios.get(apiUrl + resource, headers)
+      .then(response => {
+        userList = response.data.queryResponse.entityId
+        resolve(userList.map(user => user.$))
       })
-      .catch(err => {reject(err)});
-    });
-    fulfill(true)
-
-  });
+      .catch(err => {
+        reject(err)
+      })
+  })
 }
 
-getUsers(api + resource + query, headers)
-.then(users => {console.log(users)})
-.catch(err => {console.error(err)})
+// getUsers(api + resource + query, headers)
+// .then(users => {console.log(users)})
+// .catch(err => {console.error(err)})
+
+getClientIds(apiUrl, headers)
+  .then(users => { console.log(users) })
+  .catch(err => { errorHandler(err) })
