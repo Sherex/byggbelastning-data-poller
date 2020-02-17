fd559772af411c09d3bcfdae0adde858a9c91a4a
Ingar Helgesen <ingar.helgesen@t-fk.no>
2020-02-17 20:36:27 +0100

    Cleaned up code & worked on client parsing

--- index.js
+++ index.js
@@ -3,29 +3,50 @@ const db = require('./lib/influxdb-interaction')
 const getClientsPerLocation = require('./lib/get-clients-per-location')
 const getClients = require('./lib/get-clients-v2')
 const errorHandler = require('./lib/error-handler')
+const macVendorStats = require('./lib/stats/macVendorStats')
+
+// DEBUG
+const perLocation = false
+const writeToDb = false
 
 // Environment variables
 const basicAuth = Buffer.from(process.env.USERNAME + ':' + process.env.PASSWORD, 'utf-8').toString('base64')
 const apiUrl = process.env.PRIME_API_URL
-const primeReportName = process.env.PRIME_REPORT_NAME
+const primeReportNamePerFloor = process.env.PRIME_REPORT_NAME_CLIENT_PER_FLOOR
+const primeReportNameClients = process.env.PRIME_REPORT_NAME_CLIENTS
 
 const headers = {
   headers: {
     Authorization: 'Basic ' + basicAuth
   }
-};
+}
 
-(async () => {
-  console.log( await getClients(apiUrl, headers, primeReportName) )
+
+;(async () => {
+  if (perLocation) {
+    console.log(await getClientsPerLocation(apiUrl, headers, primeReportNamePerFloor))
+  } else {
+    const clients = await getClients(apiUrl, headers, primeReportNameClients)
+    // console.log(clients)
+    // Get number of devices by vendor
+    console.log(macVendorStats(clients))
+  }
 })()
+// */
 
-
-
-/*
-getClientsPerLocation(apiUrl, headers, primeReportName)
-  .then(async data => {
-    await db.createDb()
-    await db.writeToDb(data)
-  })
-  .catch(err => { errorHandler(err) })
-*/
+if (perLocation && writeToDb) {
+  getClientsPerLocation(apiUrl, headers, primeReportNamePerFloor)
+    .then(async data => {
+      await db.createDb()
+      await db.writeToDb(data)
+    })
+    .catch(err => { errorHandler(err) })
+} else if (!perLocation && writeToDb) {
+  getClients(apiUrl, headers, primeReportNameClients)
+    .then(async data => {
+      await db.createDb()
+      await db.writeToDb(data)
+    })
+    .catch(err => { errorHandler(err) })
+}
+// */
