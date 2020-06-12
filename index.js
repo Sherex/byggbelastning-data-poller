9d486225a72acb87a90f03320b955744ad22b924
Ingar Helgesen <ingar@i-h.no>
2020-06-12 09:08:26 +0200

    Env var name update

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const getClients = require('./lib/get-clients-v2')
 const errorHandler = require('./lib/error-handler')
 const macVendorStats = require('./lib/stats/macVendorStats')
 
+// TODO: Convert all functions into factory functions, one for MSE and one for Prime
+
 // DEBUG
 const perLocation = true
-const writeToDb = true
+const writeToDb = false
 
 // Environment variables
-const basicAuth = Buffer.from(process.env.USERNAME + ':' + process.env.PASSWORD, 'utf-8').toString('base64')
+const basicAuth = Buffer.from(process.env.PRIME_USERNAME + ':' + process.env.PRIME_PASSWORD, 'utf-8').toString('base64')
 const apiUrl = process.env.PRIME_API_URL
 const primeReportNamePerFloor = process.env.PRIME_REPORT_NAME_CLIENT_PER_FLOOR
 const primeReportNameClients = process.env.PRIME_REPORT_NAME_CLIENTS
@@ -23,14 +25,15 @@ const headers = {
 
 ;(async () => {
   if (perLocation) {
-    console.log(await getClientsPerLocation(apiUrl, headers, primeReportNamePerFloor))
+    const data = await getClientsPerLocation(apiUrl, headers, primeReportNamePerFloor)
+    console.log(data.length)
   } else {
     const clients = await getClients(apiUrl, headers, primeReportNameClients)
-    // console.log(clients)
+    console.log(clients)
     // Get number of devices by vendor
-    console.log(macVendorStats(clients))
+    // console.log(macVendorStats(clients))
   }
-})()
+})().catch(err => { errorHandler(err) })
 // */
 
 if (perLocation && writeToDb) {
