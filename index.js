c42d9d16ddcc0186f70805d2734446bf74d13021
Ingar Helgesen <ingar@i-h.no>
2020-06-12 10:45:24 +0200

    Created prime factoryFunction

--- index.js
+++ index.js
@@ -1,54 +1,41 @@
 require('dotenv').config()
 const db = require('./lib/db/timescaledb-interaction')
-const getClientsPerLocation = require('./lib/prime/get-clients-per-location')
-const getClients = require('./lib/prime/get-clients')
+const createPrime = require('./lib/prime')
 const errorHandler = require('./lib/error-handler')
 const macVendorStats = require('./lib/stats/macVendorStats')
 
 // TODO: Convert all functions into factory functions, one for MSE and one for Prime
 
 // DEBUG
-const perLocation = true
+const perLocation = false
 const writeToDb = false
 
 // Environment variables
-const basicAuth = Buffer.from(process.env.PRIME_USERNAME + ':' + process.env.PRIME_PASSWORD, 'utf-8').toString('base64')
-const apiUrl = process.env.PRIME_API_URL
 const primeReportNamePerFloor = process.env.PRIME_REPORT_NAME_CLIENT_PER_FLOOR
 const primeReportNameClients = process.env.PRIME_REPORT_NAME_CLIENTS
 
-const headers = {
-  headers: {
-    Authorization: 'Basic ' + basicAuth
-  }
-}
+const { getClientsPerLocation, getClients } = createPrime({
+  baseUrl: process.env.PRIME_API_URL,
+  username: process.env.PRIME_USERNAME,
+  password: process.env.PRIME_PASSWORD
+})
 
 ;(async () => {
   if (perLocation) {
-    const data = await getClientsPerLocation(apiUrl, headers, primeReportNamePerFloor)
+    const data = await getClientsPerLocation(primeReportNamePerFloor)
     console.log(data.length)
-  } else {
-    const clients = await getClients(apiUrl, headers, primeReportNameClients)
-    console.log(clients)
-    // Get number of devices by vendor
-    // console.log(macVendorStats(clients))
-  }
-})().catch(err => { errorHandler(err) })
-// */
-
-if (perLocation && writeToDb) {
-  getClientsPerLocation(apiUrl, headers, primeReportNamePerFloor)
-    .then(async data => {
+    if (writeToDb) {
       await db.setupDB()
       await db.insertClientLocations(data)
-    })
-    .catch(err => { errorHandler(err) })
-} else if (!perLocation && writeToDb) {
-  getClients(apiUrl, headers, primeReportNameClients)
-    .then(async data => {
+    }
+  } else {
+    const data = await getClients(primeReportNameClients)
+    console.log(data.length)
+    if (writeToDb) {
       await db.setupDB()
       await db.writeToDb(data)
-    })
-    .catch(err => { errorHandler(err) })
-}
-// */
+    }
+    // Get number of devices by vendor
+    // console.log(macVendorStats(data))
+  }
+})().catch(errorHandler)
