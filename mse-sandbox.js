04ebebaa105604a00158d4345ea1e44df6a05ded
Ingar Helgesen <ingar@i-h.no>
2020-09-30 14:25:32 +0200

    Fixed insert queries

--- mse-sandbox.js
+++ mse-sandbox.js
@@ -8,26 +8,29 @@ const mse = createMse({
   password: process.env.MSE_PASSWORD
 })
 
-;(async () => {
-  logger('debug', ['mse-sandbox', 'getting existing clients from DB'])
-  const oldClients = await db.query('SELECT COUNT(cid) FROM clients_coordinates')
-  logger('debug', ['mse-sandbox', 'updating client locations'])
-  await mse.updateClientLocations()
-  logger('debug', ['mse-sandbox', 'getting total clients from DB'])
-  const clients = await db.query('SELECT COUNT(cid) FROM clients_coordinates')
-  logger('debug', ['mse-sandbox', 'old clients in db', oldClients.rows[0].count, 'total clients', clients.rows[0].count])
-  await db.close({ immediate: true })
-})().catch(error => {
-  console.error(error)
-})
-
 // ;(async () => {
-//   const locationsData = await mse.updateLocations()
-//   const { rows: locations } = await db.query('SELECT imageBase64,mapHierarchyString FROM locations')
-//   const html = `<html><body>${locations.map(loc => `<h1>${loc.maphierarchystring}</h1><img src="data:image/jpeg;base64,${loc.imagebase64}" style="width: 50em;"><br>`)}</body></html>`
-//   require('fs').writeFileSync('./sample-data/mse-data/test.html', html)
-//   // console.log(locations.map(loc => ({
-//   //   ...loc,
-//   //   imageBase64: loc.imageBase64.substr(0, 20)
-//   // })))
-// })()
+//   logger('debug', ['mse-sandbox', 'getting existing clients from DB'])
+//   const oldClients = await db.query('SELECT COUNT(cid) FROM clients_coordinates')
+//   logger('debug', ['mse-sandbox', 'updating client locations'])
+//   await mse.updateClientLocations()
+//   logger('debug', ['mse-sandbox', 'getting total clients from DB'])
+//   const clients = await db.query('SELECT COUNT(cid) FROM clients_coordinates')
+//   logger('debug', ['mse-sandbox', 'old clients in db', oldClients.rows[0].count, 'total clients', clients.rows[0].count])
+//   await db.close({ immediate: true })
+// })().catch(error => {
+//   console.error(error)
+// })
+
+;(async () => {
+  const floors = await mse.getFloors()
+  // console.log(floors)
+
+  logger('debug', ['mse-sandbox', 'inserting locations to DB'])
+  await db.insertLocation(floors)
+  logger('debug', ['mse-sandbox', 'inserting buildings to DB'])
+  await db.insertBuilding(floors)
+  logger('debug', ['mse-sandbox', 'inserting floors to DB', 'total floors', floors.length])
+  await db.insertFloor(floors)
+  // const html = `<html><body>${locations.map(loc => `<h1>${loc.maphierarchystring}</h1><img src="data:image/jpeg;base64,${loc.imagebase64}" style="width: 50em;"><br>`)}</body></html>`
+  // require('fs').writeFileSync('./sample-data/mse-data/test.html', html)
+})()
