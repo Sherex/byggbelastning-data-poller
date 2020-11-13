563f9d8c1e2792f4b8ade7d4cbc68f0886698a43
Ingar Helgesen <ingar@i-h.no>
2020-11-13 09:38:37 +0100

    Update mse-sandbox

--- mse-sandbox.js
+++ mse-sandbox.js
@@ -9,19 +9,30 @@ const mse = createMse({
   password: process.env.MSE_PASSWORD
 })
 
+// Update floors
 ;(async () => {
-  logger('debug', ['mse-sandbox', 'getting existing clients from DB'])
-  const oldClients = await db.query('SELECT COUNT(cid) FROM client_coordinate')
-  logger('debug', ['mse-sandbox', 'updating client locations'])
-  const { stats } = await mse.updateClientLocations()
-  logger('debug', ['mse-sandbox', 'success', 'total clients', stats.totalClients, 'successful', stats.success, 'failed', stats.failed])
-  logger('debug', ['mse-sandbox', 'getting total clients from DB'])
-  const clients = await db.query('SELECT COUNT(cid) FROM client_coordinate')
-  logger('debug', ['mse-sandbox', 'old clients in db', oldClients.rows[0].count, 'total clients', clients.rows[0].count])
+  logger('debug', ['mse-sandbox', 'updating floors'])
+  const floors = await mse.getFloors()
+  await db.updateFloor(floors)
+  logger('debug', ['mse-sandbox', 'success!'])
   await db.close({ immediate: true })
 })().catch(error => {
   console.error(error)
 })
+
+// ;(async () => {
+//   logger('debug', ['mse-sandbox', 'getting existing clients from DB'])
+//   const oldClients = await db.query('SELECT COUNT(cid) FROM client_coordinate')
+//   logger('debug', ['mse-sandbox', 'updating client locations'])
+//   const { stats } = await mse.updateClientLocations()
+//   logger('debug', ['mse-sandbox', 'success', 'total clients', stats.totalClients, 'successful', stats.success, 'failed', stats.failed])
+//   logger('debug', ['mse-sandbox', 'getting total clients from DB'])
+//   const clients = await db.query('SELECT COUNT(cid) FROM client_coordinate')
+//   logger('debug', ['mse-sandbox', 'old clients in db', oldClients.rows[0].count, 'total clients', clients.rows[0].count])
+//   await db.close({ immediate: true })
+// })().catch(error => {
+//   console.error(error)
+// })
 
 // Insert floors
 // ;(async () => {
