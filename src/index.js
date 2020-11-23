790a463a341b9e5b477754ad1e430fdc01882b1c
Ingar Helgesen <ingar@i-h.no>
2020-11-23 16:24:40 +0100

    Added client_count update to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { logger } = require('@vtfk/logger')
 const createMse = require('./lib/mse')
+const createPrime = require('./lib/prime')
 const db = require('./lib/db/timescaledb-interaction')
 
 const mse = createMse({
@@ -8,18 +9,34 @@ const mse = createMse({
   password: process.env.MSE_PASSWORD
 })
 
+const { getClientsPerLocation } = createPrime({
+  baseUrl: process.env.PRIME_API_URL,
+  username: process.env.PRIME_USERNAME,
+  password: process.env.PRIME_PASSWORD
+})
+
 ;(async () => {
-  logger('debug', ['mse-sandbox', 'getting existing clients from DB'])
+  logger('debug', ['index', 'getting existing clients from DB'])
   const oldClients = await db.query('SELECT COUNT(cid) FROM client_coordinate')
-  logger('debug', ['mse-sandbox', 'updating location names'])
+  logger('debug', ['index', 'updating location names'])
   const floors = await mse.getFloors()
   await db.updateFloor(floors)
-  logger('debug', ['mse-sandbox', 'updating client locations'])
+
+  try {
+    logger('debug', ['index', 'inserting client_count to DB...'])
+    const data = await getClientsPerLocation(process.env.PRIME_REPORT_NAME_CLIENT_PER_FLOOR)
+    const result = await db.insertClientCount(data)
+    logger('info', ['inserted', result.rowCount, 'rows to client_count'])
+  } catch (error) {
+    logger('error', ['failed to insert count to client_count', 'ignoring', 'error', error.message])
+  }
+
+  logger('debug', ['index', 'updating client locations'])
   const { stats } = await mse.updateClientLocations()
-  logger('debug', ['mse-sandbox', 'success', 'total clients', stats.totalClients, 'successful', stats.success, 'failed', stats.failed])
-  logger('debug', ['mse-sandbox', 'getting total clients from DB'])
+  logger('info', ['index', 'success', 'total clients', stats.totalClients, 'successful', stats.success, 'failed', stats.failed])
+  logger('debug', ['index', 'getting total clients from DB'])
   const clients = await db.query('SELECT COUNT(cid) FROM client_coordinate')
-  logger('debug', ['mse-sandbox', 'old clients in db', oldClients.rows[0].count, 'total clients', clients.rows[0].count])
+  logger('debug', ['index', 'old clients in db', oldClients.rows[0].count, 'total clients', clients.rows[0].count])
   await db.close({ immediate: true })
 })().catch(error => {
   console.error(error)
